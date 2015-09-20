e15c7d2cba5c8b313f399569e610ea0cf7f65887
Jeehoo Ahn <ahnbizcad@gmail.com>
2015-09-20 00:07:10 -0700

    add two components

---
+++ app/firstone/config/routes.rb
@@ -0,0 +1,4 @@
+# See https://github.com/voltrb/volt#routes for more info on routes
+
+client "/firstone",     component: 'firstone', controller: 'main', action: 'index'
+client "/firstone/hi",  component: 'firstone', controller: 'main', action: 'hi'
